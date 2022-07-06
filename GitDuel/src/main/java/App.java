6ff34e7f47bc2973593292ff7ac4456b45a0d667
Adam Wrutniak <adamwrutniak@gmail.com>
2022-07-06 22:23:28 +0200

    xxx

--- GitDuel/src/main/java/App.java
+++ GitDuel/src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hallo Adam");
+        System.out.println("Hallo Adam23");
         System.out.println("Hallo Kazik");
 
 
