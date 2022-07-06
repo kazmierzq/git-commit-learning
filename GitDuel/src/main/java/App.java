0d261b570564c8f09977dafb36e7134b6be83d26
kazmierzq <wroblewski.konrad.kk@gmail.com>
2022-07-06 22:21:02 +0200

    Pushing changes in App file to create a conflict

--- GitDuel/src/main/java/App.java
+++ GitDuel/src/main/java/App.java
@@ -3,8 +3,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Hallo Adam");
-        System.out.println("Hallo Kazik");
-
-
+        System.out.println("Hallo Kazik 2");
+        
     }
 }
